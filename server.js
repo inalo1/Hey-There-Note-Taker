e08a064ddf0af9e32336fb21100ba73f3c9f2049
Ivy Gabrielle Nalo <nalo.ivielle13@gmail.com>
2021-03-08 15:04:18 -0600

    fixed bug in server.js file

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static(__dirname + "/public"));
 
 //set routes
 
-require('./routes/routesAPI')(app);
+require('./routes/routesAPI');
 
 
 // const routesHTML = require("./routes/routesHTML");
